40f6024a63f4ad004100fd22f1eb6e0f941ea95b
Francesco Garofolo <francyclimber@gmail.com>
2026-06-12 13:27:57 +0200

    sockets

--- client/main.c
+++ client/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// librerie posix
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #ifdef GUI
 #include "gui.h"
@@ -7,24 +15,55 @@
 #include "cliview.h"
 #endif
 
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 8080
+
+int socket_des;
+
 int main(int argc, char const *argv[]) {
+
+    printf("Setup... ⏳\n");
+
+    printf("Socket setup \n");
+
+    socket_des = socket(PF_INET, SOCK_STREAM, 0);
+    if (socket_des == -1) {
+        printf("Error: Creation of socket \n");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+    if (connect(socket_des, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        printf("Error: Connection to server \n");
+        close(socket_des);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to the server ✅\n");
+
+    printf("Setup complete ✅\n");
+
     char buffer[255];
     int op; // codice dell'operazione selezionato dall'utente all'interno del menu
 
-    printf("Le tue opzioni: \n0) 📤 Esci \n1) 🆕 Nuova prenotazione \n2) ❌ Gestisci una vecchia prenotazione \n");
+    printf("Options: \n0) 📤 Exit \n1) 🆕 New booking  \n2) 🔧 Manage old booking \n");
     fflush(stdout);
 
     while (1) {
         fflush(stdout);
         printf("-> ");
-        
+
         fgets(buffer, sizeof(buffer), stdin);
         sscanf(buffer, "%d", &op); // estraggo il codice dell'operazione
 
         switch (op) {
         case 0:
-            printf("Sei uscito correttamente!\n");
-            fflush(stdout);
+            close(socket_des); // chiudo il socket
+            printf("Exit success!\n");
             return 0;
 
         case 1:
---
+++ server/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// posix library
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define BACKLOG 10
+#define PORT 8080
+
+
+struct connection_handler_arg{
+    int socket_id;
+};
+
+struct sockaddr_in server_addr;
+
+void *connection_handler(void *arg){
+    struct connection_handler_arg *data = (struct connection_handler_arg *)arg;
+
+    printf("ok \n");
+
+
+    return NULL;
+}
+
+int main(int argc, char const *argv[]) {
+
+    printf("Setup... ⏳\n");
+    
+    printf("Socket setup \n");
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    int general_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if(general_socket == -1){
+        printf("Error: Creation of general socket \n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(bind(general_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
+        printf("Error: bind not found \n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(general_socket, BACKLOG)){
+        printf("Error: listen not found");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Setup complete ✅\n");
+    printf("Waiting for connection \n");
+
+    int new_socket;
+    pthread_t thread;
+
+    while (1){
+        new_socket = accept(general_socket, NULL, NULL);
+        if(new_socket == -1){
+            printf("Error on accept \n");
+            continue;
+        }
+
+        printf("New connection \n");
+
+        struct connection_handler_arg *arg = malloc(sizeof(struct connection_handler_arg));
+        arg->socket_id = new_socket;
+
+        pthread_create(&thread, NULL, connection_handler, NULL);
+    }
+    
+
+    return 0;
+}
