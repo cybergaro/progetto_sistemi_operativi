b91d2f450cc00f9f2f8fb86b2fb8160f9b6ee690
Flavio <flavio.favero0505@gmail.com>
2026-06-13 10:51:31 +0200

    fx

--- server/filemanager/unixfilemanager.c
+++ server/filemanager/unixfilemanager.c
@@ -1,11 +1,52 @@
 #include "unixfilemanager.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
-int fd = open(MAP_FILE, O_RDWR, 0644);
 
-    if (fd < 0){ // caso in cui il file non esiste
-        fd = open(MAP_FILE, O_CREAT | O_TRUNC | O_RDWR, 0644);
-        if(fd < 0){
-            printf("Error map file creation\n");
-            exit(EXIT_FAILURE);
+
+
+
+int create_map(char *fname) 
+{
+    
+    int fd = open(fname, O_RDWR, 0644);  
+
+    if (fd < 0) { 
+        
+        
+        fd = open(fname, O_CREAT | O_RDWR, 0644);
+        if (fd < 0) { 
+            printf("Errore nella creazione della mappa\n");
+            return -1;
         }
-    }
+
+        
+        Posto posto_vuoto;
+        posto_vuoto.occupato = 0;           
+        posto_vuoto.num_prenotazione = 0;
+
+        int total_seats = ROWS * COLS;
+
+        
+        for(int i = 0; i < total_seats; i++) {
+            ssize_t bytes_written = write(fd, &posto_vuoto, sizeof(Posto));
+            
+            if (bytes_written != sizeof(Posto)) {
+                perror("Errore durante l'inizializzazione dei posti");
+                close(fd); 
+                return -1;
+            }
+        }   
+
+        
+        close(fd);
+        return 0; 
+    }    
+
+    
+    
+    close(fd);
+    return 0;
+}
--- server/filemanager/unixfilemanager.h
+++ server/filemanager/unixfilemanager.h
@@ -1,12 +1,15 @@
 #ifndef UNIXFILEMANAGER
 #define UNIXFILEMANAGER
+#define ROWS 10
+#define COLS 15
+
 
 typedef struct{
     unsigned short int occupato;
     unsigned int num_prenotazione;
 } Posto;
 
-int create_map(char *fname, int namesize); // crea il file che rappresenta la mappa del cinema se non esiste
+int create_map(char *fname); // crea il file che rappresenta la mappa del cinema se non esiste
 
 int seat_set_flag(int ds, char row, int col, int flag, int nbook); // permette di associare/disassociare un posto da una certa prenotazionre
 int seat_get_flag(int ds, char row, int col);
