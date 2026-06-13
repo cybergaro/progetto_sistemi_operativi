f11aedbaf46eedaa4a290847e7058868d64ede4b
Flavio <flavio.favero0505@gmail.com>
2026-06-13 11:25:02 +0200

    fx

--- server/filemanager/unixfilemanager.c
+++ server/filemanager/unixfilemanager.c
@@ -13,16 +13,16 @@ int create_map(char *fname) {
             return -1;
         }
 
-        Posto posto_vuoto;
-        posto_vuoto.occupato = 0;
-        posto_vuoto.num_prenotazione = 0;
+        Seat posto_vuoto;
+        posto_vuoto.flag = 0;
+        posto_vuoto.nbook = 0;
 
         int total_seats = ROWS * COLS;
 
         for (int i = 0; i < total_seats; i++) {
-            ssize_t bytes_written = write(fd, &posto_vuoto, sizeof(Posto));
+            ssize_t bytes_written = write(fd, &posto_vuoto, sizeof(Seat));
 
-            if (bytes_written != sizeof(Posto)) {
+            if (bytes_written != sizeof(Seat)) {
                 close(fd);
                 return -1;
             }
@@ -33,5 +33,27 @@ int create_map(char *fname) {
     }
 
     close(fd);
+    return 0;
+}
+
+
+int get_all_flag(int ds,unsigned short int matrix[ROWS][COLS])
+{
+   
+    if (lseek(ds, 0, SEEK_SET) == (off_t)-1) {
+        printf("Errore lseek in get_all_flag");
+        return -1;
+    }
+    Seat p;
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < COLS; c++) {
+            ssize_t bytes_read = read(ds, &p, sizeof(Seat));
+            if (bytes_read != sizeof(Seat)) {
+                printf("Errore durante la lettura dei posti dal file");
+                return -1;
+            }
+            matrix[r][c] = p.flag;
+        }
+    }
     return 0;
 }
--- server/filemanager/unixfilemanager.h
+++ server/filemanager/unixfilemanager.h
@@ -5,14 +5,15 @@
 
 
 typedef struct{
-    unsigned short int occupato;
-    unsigned int num_prenotazione;
-} Posto;
+    unsigned short int flag;
+    unsigned int nbook;
+} Seat;
 
 int create_map(char *fname); // crea il file che rappresenta la mappa del cinema se non esiste
 
 int seat_set_flag(int ds, char row, int col, int flag, int nbook); // permette di associare/disassociare un posto da una certa prenotazionre
 int seat_get_flag(int ds, char row, int col);
+int get_all_flag(int ds);
 
 // int get_seats_from_book(int ds, int nbook);
 
