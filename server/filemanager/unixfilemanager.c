5fa0c150f4ec7592627ba7ef8a85c2f3dce6f461
Francesco Garofolo <francyclimber@gmail.com>
2026-06-13 10:52:36 +0200

    indentazione  funzioni

--- server/filemanager/unixfilemanager.c
+++ server/filemanager/unixfilemanager.c
@@ -1,52 +1,39 @@
 #include "unixfilemanager.h"
-#include <stdlib.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+int create_map(char *fname) {
+    int fd = open(fname, O_RDWR, 0644);
 
-
-
-
-int create_map(char *fname) 
-{
-    
-    int fd = open(fname, O_RDWR, 0644);  
-
-    if (fd < 0) { 
-        
-        
+    if (fd < 0) {
         fd = open(fname, O_CREAT | O_RDWR, 0644);
-        if (fd < 0) { 
+        if (fd < 0) {
             printf("Errore nella creazione della mappa\n");
             return -1;
         }
 
-        
         Posto posto_vuoto;
-        posto_vuoto.occupato = 0;           
+        posto_vuoto.occupato = 0;
         posto_vuoto.num_prenotazione = 0;
 
         int total_seats = ROWS * COLS;
 
-        
-        for(int i = 0; i < total_seats; i++) {
+        for (int i = 0; i < total_seats; i++) {
             ssize_t bytes_written = write(fd, &posto_vuoto, sizeof(Posto));
-            
+
             if (bytes_written != sizeof(Posto)) {
                 perror("Errore durante l'inizializzazione dei posti");
-                close(fd); 
+                close(fd);
                 return -1;
             }
-        }   
+        }
 
-        
         close(fd);
-        return 0; 
-    }    
+        return 0;
+    }
 
-    
-    
     close(fd);
     return 0;
 }
